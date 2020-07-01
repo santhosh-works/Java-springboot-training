332ee2d44f6ec59433bd22a7e1066b9ff7a7fb36
santhosh-works <mailtosanthoshsubramanian@gmail.com>
2020-07-01 22:50:59 +0530

    first commit

---
+++ src/com/sans/java/feature/LearnConcepts.java
@@ -0,0 +1,11 @@
+package com.sans.java.feature;
+
+public class LearnConcepts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First Learnings");
+	}
+
+}
